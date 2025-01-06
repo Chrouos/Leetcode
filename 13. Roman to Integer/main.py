a37019d2cbd233e44538b2a37813470fb0498fa9
黃懷萱 <chrbezz0487@gmail.com>
2025-01-07 01:21:48 +0800

    13, 14

---
+++ 13. Roman to Integer/main.py
@@ -0,0 +1,31 @@
+﻿class Solution:
+    def romanToInt(self, s: str) -> int:
+        
+        roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev = 0
+        for char in s:
+            if roman_dict[char] > prev:
+                result += roman_dict[char] - 2 * prev
+            else:
+                result += roman_dict[char]
+            prev = roman_dict[char]
+            
+        return result
+    
+if __name__ == '__main__':
+    s = Solution()
+    print(s.romanToInt('III')) # 3
+    print(s.romanToInt('IV')) # 4
+    print(s.romanToInt('IX')) # 9
+    print(s.romanToInt('LVIII')) # 58
+    print(s.romanToInt('MCMXCIV')) # 1994
---
+++ 14. Longest Common Prefix/main.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        
+        if not strs: return ''
+        
+        result_prefix = ''
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return result_prefix
+            result_prefix += strs[0][i]
+        
+        return result_prefix
+    
+    
+if __name__ == '__main__':
+    strs = ["flower","flow","flight"]
+    s = Solution()
+    print(s.longestCommonPrefix(strs)) # 'fl'
+    
