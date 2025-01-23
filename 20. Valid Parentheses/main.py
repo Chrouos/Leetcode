8766c2f0b68406d74298d7787faa7ea4836ff5fe
黃懷萱 <chrbezz0487@gmail.com>
2025-01-23 13:29:58 +0800

    增加 20, 22

---
+++ 20. Valid Parentheses/main.py
@@ -0,0 +1,34 @@
+﻿class Solution:
+    def isValid(self, s: str) -> bool:
+        char_stack = []
+        char_map = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        
+        for c in s:
+            if c in char_map:
+                if not char_stack or char_stack.pop() != char_map[c]:
+                    return False
+            else:
+                char_stack.append(c)
+                
+        # If stack is empty, then all the characters are matched
+        return not char_stack
+        
+        
+if __name__ == '__main__':
+    s = Solution()
+    s_list = [
+        {"input": "()", "output": True},
+        {"input": "()[]{}", "output": True},
+        {"input": "(]", "output": False},
+        {"input": "([)]", "output": False},
+        {"input": "{[]}", "output": True},
+        {"input": "]", "output": False},
+        {"input": "[", "output": False},
+    ]
+    
+    for i in s_list:
+        assert s.isValid(i["input"]) == i["output"]
---
+++ 22. Generate Parentheses/main.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        
+        result = []
+        def parenthesis(s, left_count, right_count):
+            if left_count == 0 and right_count == 0:
+                result.append(s)
+            if left_count > 0:
+                parenthesis(s + '(', left_count - 1, right_count)
+            if right_count > left_count:
+                parenthesis(s + ')', left_count, right_count - 1)        
+        
+        parenthesis('', n, n)
+        return result
+        
+
+if __name__ == '__main__':
+    n_list = [3, 1]
+    s = Solution()
+    
+    for n in n_list:
+        print(s.generateParenthesis(n))
