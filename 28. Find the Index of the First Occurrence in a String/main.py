5b26482d0d93ab981c33419f7513a3af14c8156d
黃懷萱 <chrbezz0487@gmail.com>
2025-01-31 00:42:09 +0800

    28.

---
+++ 28. Find the Index of the First Occurrence in a String/main.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+      
+      for i in range(len(haystack) - len(needle) + 1):
+          if haystack[i:i+len(needle)] == needle:
+              return i
+      
+      
+      return -1
+        
+        
+        
+if __name__ == '__main__':
+    question_list = [
+        {'haystack': 'hello', 'needle': 'll', 'answer': 2},
+        {'haystack': 'aaaaa', 'needle': 'bba', 'answer': -1},
+        {'haystack': '', 'needle': '', 'answer': 0}
+    ]
+    
+    s = Solution()
+    for question in question_list:
+        print(s.strStr(question['haystack'], question['needle']) == question['answer'])
