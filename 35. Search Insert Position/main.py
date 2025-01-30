7a838030b259db76bd2be40f46d9591f99bfba7a
黃懷萱 <chrbezz0487@gmail.com>
2025-01-31 06:09:19 +0800

    35 easy

---
+++ 35. Search Insert Position/main.py
@@ -0,0 +1,22 @@
+﻿from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+    
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+    
+    
+if __name__ == '__main__':
+    s = Solution()
+    
+    question = [
+        {'nums': [1,3,5,6], 'target': 5, 'answer': 2},
+        {'nums': [1,3,5,6], 'target': 2, 'answer': 1},
+        {'nums': [1,3,5,6], 'target': 7, 'answer': 4},
+    ]
+    
+    for q in question:
+        assert s.searchInsert(q['nums'], q['target']) == q['answer']
