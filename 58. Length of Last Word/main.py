40e400bee7716721a7bf80a070a517f37895ee01
黃懷萱 <chrbezz0487@gmail.com>
2025-02-11 14:44:42 +0800

    58 easy

---
+++ 58. Length of Last Word/main.py
@@ -0,0 +1,22 @@
+﻿class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        split_s = s.split()
+        max_len = 0
+        for word in split_s:
+            max_len = max(max_len, len(word))
+            
+        return max_len
+
+if __name__ in "__main__":
+    
+    s = Solution()
+    question_list = [
+        {"input": "Hello World", "output": 5},
+        {"input": " ", "output": 0},
+        {"input": "a ", "output": 1},
+        {"input": "   fly me   to   the moon  ", "output": 4},
+    ]
+    
+    for question in question_list:
+        print(s.lengthOfLastWord(question["input"]) == question["output"])
