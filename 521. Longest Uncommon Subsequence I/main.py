762041d97e0e4bfc47b600c86c3faddd00164505
黃懷萱 <chrbezz0487@gmail.com>
2025-02-13 14:44:04 +0800

    521 easy 最長 subsequence

---
+++ 521. Longest Uncommon Subsequence I/main.py
@@ -0,0 +1,21 @@
+﻿class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        
+        if a == b:
+            return -1
+        else:
+            return max(len(a), len(b))
+        
+    
+if __name__ == '__main__':
+    
+    s = Solution()
+    question_list = [
+        {"input": ("aba", "cdc"), "output": 3},
+        {"input": ("aaa","aaa"), "output": -1},
+        {"input": ("aaa","aaa"), "output": -1},
+        {"input": ("abcde", "defgk"), "output": 5},
+    ]
+    
+    for question in question_list:
+        print(s.findLUSlength(*question['input']) == question['output'])
