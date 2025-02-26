144f55a912ce0b10e94be95825124bd35853d57c
黃懷萱 <chrbezz0487@gmail.com>
2025-02-26 23:55:26 +0800

    2778 easy 篩選數

---
+++ 2778. Sum of Squares of Special Elements /main.py
@@ -0,0 +1,18 @@
+﻿from typing import List
+
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+    
+        n = len(nums)
+        return sum( x * x for i, x in enumerate(nums) if ( n % ( i + 1 ) == 0 ) )
+    
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [1, 2, 3, 4], "output": 21},
+        {"input": [2, 7, 1, 19, 18, 3], "output": 63},
+    ]
+    for question in question_list:
+        expected = question["output"]
+        result = s.sumOfSquares(question["input"])
+        print(f"[{expected == result}] input: {question['input']} expected: {expected}, result: {result}")
