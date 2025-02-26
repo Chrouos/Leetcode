27e57461ee0b5bfd0fe329855380c41570f8a587
黃懷萱 <chrbezz0487@gmail.com>
2025-02-27 02:10:40 +0800

    1588 eazy

---
+++ 1588. Sum of All Odd Length Subarrays/main.py
@@ -0,0 +1,28 @@
+﻿from typing import List
+
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        
+        n = len(arr)
+        total = 0
+        for i in range(n):
+            left_count = i + 1  
+            right_count = n - i 
+            odd_count = ((left_count * right_count) + 1) // 2
+            total += arr[i] * odd_count 
+        
+        return total
+
+    
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [1, 4, 2, 5, 3], "output": 58},
+        {"input": [1, 2], "output": 3},
+        {"input": [10, 11, 12], "output": 66},
+    ]
+    
+    for question in question_list:
+        expected = question["output"]
+        result = s.sumOddLengthSubarrays(question["input"])
+        print(f"[{result == expected}] input: {question['input']}, output: {result}, expected: {expected}")
