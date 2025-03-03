c995065dfe85ee3c85b8897572d2134dba3eebf6
黃懷萱 <chrbezz0487@gmail.com>
2025-03-03 23:33:00 +0800

    977 easy 簡單 排序

---
+++ 977. Squares of a Sorted Array/main.py
@@ -0,0 +1,17 @@
+﻿from typing import List
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        return sorted([x * x for x in nums])
+        
+        
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [-4,-1,0,3,10], "output": [0,1,9,16,100]},
+    ]
+    
+    for question in question_list:
+        output = s.sortedSquares(question["input"])
+        print(f"[{output == question['output']}] input: {question['input']}, output: {output}, expected: {question['output']}")
