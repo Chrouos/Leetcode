0121c330eb21bc3bd1544de5dee5ce2a98d49559
黃懷萱 <chrbezz0487@gmail.com>
2025-03-03 21:38:33 +0800

    88 easy 填空格 注意不是回傳而是修改 num1, 不難

---
+++ 88. Merge Sorted Array/main.py
@@ -0,0 +1,34 @@
+﻿from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        result = []
+        i, j = 0, 0
+        
+        while i < m and j < n:
+            if nums1[i] < nums2[j]:
+                result.append(nums1[i])
+                i += 1
+            else:
+                result.append(nums2[j])
+                j += 1
+                
+        result.extend(nums1[i:m])
+        result.extend(nums2[j:n])
+        nums1[:m+n] = result
+
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [[1,2,3,0,0,0], 3, [2,5,6], 3], "output": [1,2,2,3,5,6]},
+        # {"input": [[1], 1, [], 0], "output": [1]},
+        # {"input": [[0], 0, [1], 1], "output": [1]},
+    ]
+    
+    for question in question_list:
+        output = s.merge(*question["input"])
+        print(f"[{output == question['output']}] input: {question['input']}, output: {output}, expected: {question['output']}")
