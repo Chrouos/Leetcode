c9362445d5e8af2d0ab21f45bb7b7241da1d10b4
黃懷萱 <chrbezz0487@gmail.com>
2025-03-05 01:56:28 +0800

    medium pow 3 知道理論後就不難

---
+++ 1780. Check if Number is a Sum of Powers of Three/main.py
@@ -0,0 +1,25 @@
+﻿class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        
+        # 轉換成 3 進位
+        while n > 0:
+            if n % 3 == 2: return False # 3進位的數字中，不能有 2
+            n //= 3
+        
+        return True
+        
+        
+        
+        
+        
+if __name__ == "__main__":
+    sol = Solution()
+    question_list = [
+        {"input": 12, "output": True},
+        {"input": 91, "output": True},
+        {"input": 21, "output": False},
+    ]
+    
+    for question in question_list:
+        output = sol.checkPowersOfThree(question["input"])
+        print(f"[{output == question['output']}] input: {question['input']}, output: {output}, expected: {question['output']}")
