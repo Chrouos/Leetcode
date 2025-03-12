ec380d7eff849959dd7baee6c60e6873bc3d8583
黃懷萱 <chrbezz0487@gmail.com>
2025-03-12 21:03:39 +0800

    2529 easy 簡單找出 positive & negative 數量

---
+++ 2529. Maximum Count of Positive Integer and Negative Integer/main.py
@@ -0,0 +1,28 @@
+﻿from typing import List
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        
+        positive_count = 0
+        negative_count = 0
+        for num in nums:
+            if num > 0:
+                positive_count += 1
+            elif num < 0:
+                negative_count += 1
+        return max(positive_count, negative_count)
+        
+
+if __name__ == "__main__":
+    s  = Solution()
+    question_list = [
+        {"input": [-2,-1,-1,1,2,3], "output": 3},
+        {"input": [-3,-2,-1,0,0,1,2], "output": 3},
+        {"input": [5,20,66,1314], "output": 4},
+        {"input": [-1,-1,-1,-1], "output": 4},
+        {"input": [1,2,3,4], "output": 4},
+        {"input": [], "output": 0},
+    ]
+    
+    for question in question_list:
+        output = s.maximumCount(question['input'])
+        print(f"[{question['output'] == output}], input: {question['input']}, output: {output}")
