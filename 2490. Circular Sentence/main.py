ca40ab79b77525e60ade422191026dabca63dfae
黃懷萱 <chrbezz0487@gmail.com>
2025-04-01 03:32:40 +0800

    [add] 2490 可以前後接起來的句子

---
+++ 2490. Circular Sentence/main.py
@@ -0,0 +1,28 @@
+﻿
+
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        words = sentence.split(" ")
+        if words[0][0] != words[-1][-1]:
+            return False
+        
+        for i in range(len(words) - 1):
+            if words[i][-1] != words[i+1][0]:
+                return False
+        
+        
+        
+        return True
+        
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": "leetcode exercises sound delightful", "output": True},
+        {"input": "eetcode", "output": True},
+        {"input": "Leetcode is cool", "output": False},
+        {"input": "leetcode", "output": True},
+    ]
+
+    for question in question_list:
+        output = s.isCircularSentence(question['input'])
+        print(f"[{question['output'] == output}] input: {question['input']}, output: {output}, expected: {question['output']}")
