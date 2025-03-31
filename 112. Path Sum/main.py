8f655383cbf2dd01db2549eb4a0706285b1500de
黃懷萱 <chrbezz0487@gmail.com>
2025-04-01 02:47:09 +0800

    [add] 112. DFS to check the sum of target is exist

---
+++ 112. Path Sum/main.py
@@ -0,0 +1,53 @@
+﻿from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+    def list_to_tree(self, lst):
+        if not list: None
+        
+        root = TreeNode(lst[0])
+        queue = [root]
+        index = 1
+        while queue:
+            node = queue.pop(0)
+            if node is None: continue
+            if index >= len(lst): break
+            
+            node.left = TreeNode(lst[index]) if lst[index] is not None else None
+            queue.append(node.left)
+            index += 1
+            
+            if index >= len(lst): break
+            node.right = TreeNode(lst[index]) if lst[index] is not None else None
+            queue.append(node.right)
+            index += 1
+                
+        return root
+    
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        
+        def dfs(node, target):
+            if not node: return False
+            
+            if not node.left and not node.right: return target == node.val
+            return dfs(node.left, target - node.val) or dfs(node.right, target - node.val)
+
+        return dfs(root, targetSum)
+
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [[5,4,8,11,None,13,4,7,2,None,None,None,1], 22], "output": True},
+        {"input": [[1,2,3], 5], "output": False},
+    ]
+
+    for question in question_list:
+        p = TreeNode().list_to_tree(question['input'][0])
+        output = s.hasPathSum(p, question['input'][1])
+        
+        print(f"[{question['output'] == output}] input: {question['input']}, output: {output}, expected: {question['output']}")
---
+++ 3191. Minimum Operations to Make Binary Array Elements Equal to One I/main.py
@@ -0,0 +1,31 @@
+﻿# https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i/?envType=daily-question&envId=2025-03-19
+from typing import List
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        
+        if sum(nums) == len(nums): return 0
+        
+        nums_copy = nums.copy()
+        count = 0
+        for i in range(len(nums_copy)):
+            if nums_copy[i] == 0 and i + 2 < len(nums_copy):
+                for j in range(i,  i + 3):
+                    nums_copy[j] ^= 1
+                count += 1
+                    
+        if count != 0 and sum(nums_copy) == len(nums_copy):
+            return count  
+        else: return -1
+        
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [[0,1,1,1,0,0]], "output": 3},
+        {"input": [[0,1,1,1]], "output": -1},
+        {"input": [[0,0,0]], "output": 1},
+    ]
+    
+    for question in question_list:
+        output = s.minOperations(*question['input'])
+        print(f"[{output == question['output']}] input: {question['input']}, output: {output}, expected: {question['output']}")
