9d6d86c82d49ac8fdfa7e02dd2afe3ad7acd59aa
黃懷萱 <chrbezz0487@gmail.com>
2025-04-23 23:32:03 +0800

    .

---
+++ 189. Rotate Array/main.py
@@ -0,0 +1,33 @@
+﻿from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
+        if k == 0:
+            return
+        
+        nums_copy = nums.copy()
+        n = len(nums)
+        k %= n 
+        
+        for i in range(n):
+            nums[i] = nums_copy[(i - k) % n]
+            
+if __name__ == "__main__":
+    s = Solution()
+    question_list = [
+        {"input": [1,2,3,4,5,6,7], "k": 3, "output": [5,6,7,1,2,3,4]},
+        {"input": [-1,-100,3,99], "k": 2, "output": [3,99,-1,-100]},
+        {"input": [1,2], "k": 3, "output": [2,1]},
+        {"input": [1], "k": 0, "output": [1]},
+        {"input": [], "k": 0, "output": []},
+    ]
+    
+    for question in question_list:
+        result = question["input"].copy()
+        s.rotate(result, question["k"])
+        print(f"[{question['output'] == result}] input: {question['input']}, k: {question['k']}, output: {result}")
---
+++ test.py
@@ -0,0 +1,39 @@
+﻿from rich.progress import (
+    Progress,
+    SpinnerColumn,
+    TextColumn,
+    BarColumn,
+    TaskProgressColumn,
+    MofNCompleteColumn,
+    TransferSpeedColumn,
+    TimeElapsedColumn,
+    TimeRemainingColumn,
+)
+
+class RichProgress:
+    def __init__(self, iterable, description="處理中..."):
+        self.iterable = iterable
+        self.description = description
+        self.progress = Progress(
+            SpinnerColumn(),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            TaskProgressColumn(),
+            MofNCompleteColumn(),
+            TransferSpeedColumn(),
+            TimeElapsedColumn(),
+            TimeRemainingColumn(),
+        )
+
+    def __iter__(self):
+        with self.progress as progress:
+            task = progress.add_task(f"[cyan]{self.description}", total=len(self.iterable))
+            for item in self.iterable:
+                yield item
+                progress.update(task, advance=1)
+
+from time import sleep
+
+for i in RichProgress(range(1000), description="處理資料中..."):
+    # 做你的事
+    sleep(0.01)  # 可加可不加
