7a3b285a4ee124a9422559b40b89afd6d3c78e0e
黃懷萱 <chrbezz0487@gmail.com>
2025-04-24 18:09:47 +0800

    238 把左邊先* 右邊*一次，把所有結果記錄下來就好

---
+++ 238. Product of Array Except Self/main.py
@@ -0,0 +1,33 @@
+﻿from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        n = len(nums)
+        result = [1] * n
+        left_product = 1
+        for i in range(n):
+            result[i] = left_product
+            left_product *= nums[i]
+        
+        right_product = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= right_product
+            right_product *= nums[i]
+        return result
+        
+if __name__ == "__main__":
+    sol = Solution()
+    question_list = [
+        {"input": [1, 2, 3, 4], "output": [24, 12, 8, 6]},
+        {"input": [-1, 1, 0, -3, 3], "output": [0, 0, 9, 0, 0]},
+        {"input": [1], "output": [1]},
+        {"input": [1, 2], "output": [2, 1]},
+    ]
+    
+    for question in question_list:
+        input_data = question["input"]
+        expected_output = question["output"]
+        
+        # Call the function with the input data
+        result = sol.productExceptSelf(input_data)
+        print(f"[{result == expected_output}] input: {input_data}, output: {result}, expected_output: {expected_output}")
